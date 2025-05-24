dd080314b0f7814ac91a8d341241ded3a833816d
7893 <etc@is20.net>
2025-05-24 22:18:44 +0800

    chore: Remove Terraform configuration as per decision to use manual/CLI management

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-# novascope/terraform/backend.tf
-
-terraform {
-  backend "gcs" {
-    bucket  = "ns-gcs-unified-sigma-outcome" // 指向您已创建的统一 GCS 桶名称
-    prefix  = "tfstate/novascope"            // 状态文件在此桶内的路径
-  }
-}
